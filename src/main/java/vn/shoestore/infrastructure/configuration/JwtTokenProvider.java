0952331b76e8d36d7ddb67ae749e56ab860f3a6b
lthung1 <167729697+lthung1@users.noreply.github.com>
2024-06-12 00:04:04 +0700

    jwt filter chain and @UseCase

---
+++ src/main/java/vn/shoestore/infrastructure/configuration/JwtTokenProvider.java
@@ -0,0 +1,60 @@
+package vn.shoestore.infrastructure.configuration;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+  private final String ACCESS_TOKEN_CLAIM = "access_token";
+
+  @Value("${vn.shoe_store.secret}")
+  private String jwtSecret;
+
+  @Value("${vn.shoe_store.secret.jwt_expiration_ms}")
+  private int jwtExpirationMs;
+
+  public String generateJwtToken(String accessToken) {
+    return Jwts.builder()
+        .claim(ACCESS_TOKEN_CLAIM, accessToken)
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+        .signWith(SignatureAlgorithm.HS256, jwtSecret.getBytes(StandardCharsets.UTF_8))
+        .compact();
+  }
+
+  public boolean validateJwtToken(String authToken) {
+    try {
+      Jwts.parser()
+          .setSigningKey(jwtSecret.getBytes(StandardCharsets.UTF_8))
+          .parseClaimsJws(authToken);
+      return true;
+    } catch (SignatureException e) {
+      log.warn("Invalid JWT signature: {}", e.getMessage());
+    } catch (MalformedJwtException e) {
+      log.warn("Invalid JWT token: {}", e.getMessage());
+    } catch (ExpiredJwtException e) {
+      log.warn("JWT token was expired: {}", e.getMessage());
+    } catch (UnsupportedJwtException e) {
+      log.warn("JWT token is unsupported: {}", e.getMessage());
+    } catch (IllegalArgumentException e) {
+    }
+    return false;
+  }
+
+  public String getAccessTokenFromJwtToken(String jwt) {
+    Claims c =
+        (Claims)
+            Jwts.parserBuilder()
+                .setSigningKey(jwtSecret.getBytes(StandardCharsets.UTF_8))
+                .build()
+                .parse(jwt)
+                .getBody();
+    return (String) c.get(ACCESS_TOKEN_CLAIM);
+  }
+}
